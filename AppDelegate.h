472217e8a86fba2361454449e07971f1e39a3bb1
asqq131 <mac@mac-mini.local>
2016-09-02 10:35:07 +0800

    项目创建

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideslipMenuDemo
+//
+//  Created by mac on 16/9/2.
+//  Copyright © 2016年 黄志武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SideslipMenuDemo
+//
+//  Created by mac on 16/9/2.
+//  Copyright © 2016年 黄志武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
